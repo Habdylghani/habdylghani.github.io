538525967ab0b18d6b154e019c507fd0ab8ef273
Your Name <houbbadi.abdelghani@gmail.com>
2022-10-14 01:20:21 -0500

    adding inheritance lab

---
+++ inheritance/code.js
@@ -0,0 +1,19 @@
+
+String.prototype.filter = function (words) {
+    return this.split(" ")
+      .filter((str) => !words.includes(str))
+      .join(" ");
+  };
+
+Array.prototype.bubbleSort = function () {
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < this.length - i; j++) {
+        if (this[j] > this[j + 1]) {
+          let temp = this[j + 1];
+          this[j + 1] = this[j];
+          this[j] = temp;
+        }
+      }
+    }
+    return this;
+  };
